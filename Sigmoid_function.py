35349cb27ecb0cd1fe7439d1105cf873e1ee4d32
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-05-18 01:43:51 +0900

    Add files via upload

---
+++ Sigmoid_function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pylab as plt
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == '__main__':
+    X = np.arange(-5.0, 5.0, 0.1)
+    Y = sigmoid(X)
+    plt.plot(X, Y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
