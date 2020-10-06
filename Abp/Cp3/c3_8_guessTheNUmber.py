6c8f7c4c4b98f397fbc9239ab19c924cfd573364
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-10-07 00:04:40 +0900

    Add files via upload

---
+++ Abp/Cp3/c3_8_guessTheNUmber.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# 数当てゲーム
+import random
+secret_number = random.randint(1, 20)
+print('１から２０までの数を当ててください')
+
+# 6回聞く
+for guess_taken in range(1, 7):
+    print('数を入力してください')
+    guess = int(input())
+    
+    if guess < secret_number:
+        print('小さいです')
+    elif guess > secret_number:
+        print('大きいです')
+    else:
+        break  # 当たり!
+
+if guess == secret_number:
+    print('当たり!' + str(guess_taken) + '回で当たりました')
+else:
+    print('残念、正解は' + str(secret_number) + 'でした')
