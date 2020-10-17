e9caa0b11775ded2d42d943e157c826cb487a4fb
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-10-18 01:04:58 +0900

    Add files via upload

---
+++ Abp/Cp5/c5_1_5_characterCount.py
@@ -0,0 +1,8 @@
+messsage = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in messsage:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
