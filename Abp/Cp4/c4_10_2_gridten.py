19f3288575db9e9048df09f5faeed8449c54e0b5
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-10-17 16:14:04 +0900

    Add files via upload

---
+++ Abp/Cp4/c4_10_2_gridten.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+def print_grid(grid):
+    width = len(grid)
+    height = len(grid[0])
+    for y in range(height):
+        for x in range(width):
+            print(grid[x][y], end='')
+        print()
+
+print_grid(grid)
