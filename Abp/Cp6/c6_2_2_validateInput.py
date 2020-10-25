d9aa0ba69760045678d584d9004048f35e890ede
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2020-10-26 00:42:50 +0900

    Add files via upload

---
+++ Abp/Cp6/c6_2_2_validateInput.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+while True:
+    print('年齢を入力してください：')
+    age = input()
+    if age.isdecimal():
+        break
+    print('年齢は数字で入力してください(英数字のみ)：')
+
+while True:
+    print('新しいパスワードを入力してください(英数字のみ)：')
+    password = input()
+    if password.isalnum():
+        break
+    print('パスワードは英数字で入力してください：')
