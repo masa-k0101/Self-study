9a4cdbbdf38f8c074cc3b77015f94403219f2b36
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2021-01-01 21:42:56 +0900

    Update 2-3-1_Seed_classification.py

--- Mwp/Cp2/2-3-1_Seed_classification.py
+++ Mwp/Cp2/2-3-1_Seed_classification.py
@@ -1,7 +1,8 @@
 #import scipy as sp
 import pandas as pd
 #import numpy as np
-from sklearn.model_selection import cross_val_score,LeaveOneOut,KFold,StratifiedKFold
+from sklearn.model_selection import cross_val_score,LeaveOneOut,KFold
+#from sklearn.model_selection import StratifiedKFold
 from matplotlib import pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
