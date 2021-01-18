33525ad6120fcd2d1e99c582addffcf8f9a45a79
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2021-01-18 22:58:54 +0900

    Add files via upload

---
+++ Abp/Cp16/c16_5_2_sendDuesReminders.py
@@ -0,0 +1,23 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+import openpyxl, smtplib, sys
+
+# スプレッドシートを開き、最近の支払い状況を取得
+wb = openpyxl.load_workbook('duesRecords.xlsx')
+sheet = wb.get_sheet_by_name('Sheet1')
+
+last_col = sheet.get_highest_column()
+latest_month = sheet.cell(row=1, column=last_col).value
+
+# 会員の支払い状況を調べる
+unpaid_members = {}
+for r in range(2, sheet.get_highest_roq() + 1):
+    payment = sheet.cell(row=r, column=last_col).value
+    if payment != 'paid':
+        name = sheet.cell(row=r, column=1).value
+        email = sheet.cell(row=r, column=2).value
+        unpaid_members[name] = email
+
+# TODO: メールアカウントにログインする
+# TODO: リマインダーメールを送信する
