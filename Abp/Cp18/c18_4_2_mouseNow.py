6dd50376cb9df2191fd2e8f10c6e7c7557873d19
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2021-01-30 11:59:13 +0900

    Add files via upload

---
+++ Abp/Cp18/c18_4_2_mouseNow.py
@@ -0,0 +1,11 @@
+#! python3
+# mouseNow.py - マウスカーソル座標を表示する
+ 
+import pyautogui
+print('中断するにはCtrl-Cを入力してください')
+
+try:
+    while True:
+        # TODO: マウス座標を取得して表示する
+except KeyboardInterrupt:
+    print('\n終了')
