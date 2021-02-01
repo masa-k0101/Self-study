43ae53d8ce9a2189f5da443fb2fda6a5c5efc949
masa-k0101 <65117419+masa-k0101@users.noreply.github.com>
2021-02-02 00:55:42 +0900

    Add files via upload

---
+++ Abp/Cp18/c18_10_1_formFiller.py
@@ -0,0 +1,30 @@
+#! python3
+# formFillerw.py - フォームの自動入入力
+ 
+import pyautogui, time
+
+# 以下の値は実際に調べたものに置き換えること
+name_field = (648, 319)
+submit_button = (651, 817)
+submit_button_color = (75, 141, 249)
+submit_another_link = (760, 224)
+
+# TODO: ユーザーがスクリプトを中断する機会を与える
+
+# TODO: フォームページが読み込まれるのを待つ
+
+# TODO: Name欄を入力する
+
+# TODO: Greatest Fear(s)を入力する
+
+# TODO: Source of Wizard Powers欄を選択する
+
+# TODO: RoboCop欄を選択する
+
+# TODO: Additional Comments欄を入力する
+
+# TODO: Submitをクリックする
+
+# TODO: 次のページが読み込まれるを待つ
+
+# TODO: Submit anotyher responceリンクをクリックする
